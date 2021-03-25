3e4571421b90f1cef84e65296d2135cc6c1464f7
alexzhang <1642114555@qq.com>
2021-03-26 00:22:07 +0800

    Web APIs 完结

---
+++ 5-WebAPI/code/100-swiper的使用/js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    // Initialize Swiper
+    var swiper = new Swiper(".swiper-container", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+})
