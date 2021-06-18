cd3b3242c159dcbbf07cbe7d9d9b508ea3779469
alexzhang <1642114555@qq.com>
2021-06-18 13:07:05 +0800

    小程序 Day 8 part 1

--- 11-微信小程序/code/1-mp-demo01/components/test01/test.js
+++ 11-微信小程序/code/1-mp-demo01/components/test01/test.js
@@ -4,20 +4,41 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    max: {
+      type: Number,
+      value: 10
+    },
+    min: Number
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    count: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+    addCount() {
+      if (this.data.count >= this.properties.max) {
+        return;
+      }
+      this.setData({
+        count: this.data.count + 1
+      })
+      this._showCount()
+    },
+    _showCount() {
+      wx.showToast({
+        title: 'count的值：' + this.data.count,
+        icon: "none"
+      })
+    }
+  },
+  // options: {
+  //   styleIsolation: "apply-shared"
+  // }
 })
