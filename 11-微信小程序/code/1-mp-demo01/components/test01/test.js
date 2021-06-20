fca5050dc8fcafffda1c3fbd17e17286f4a87c14
alexzhang <1642114555@qq.com>
2021-06-20 19:43:41 +0800

    小程序 Day 9 part 1

--- 11-微信小程序/code/1-mp-demo01/components/test01/test.js
+++ 11-微信小程序/code/1-mp-demo01/components/test01/test.js
@@ -10,12 +10,37 @@ Component({
     },
     min: Number
   },
+  options: {
+    pureDataPattern: /^_/
+  },
+  lifetimes: {
+    created: function() {
+      wx.showToast({
+        title: '组件创建完毕！',
+        icon: 'none'
+      })
+    }
+  },
+  pageLifetimes: {
+    show: function() {
+      this.setData({
+        r: this._randomNumber(),
+        g: this._randomNumber(),
+        b: this._randomNumber(),
+        allColors: [this.data.r, this.data.g, this.data.b]
+      })
+    }
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
-    count: 0
+    count: 0,
+    r: 0,
+    g: 0,
+    b: 0,
+    allColors: [0, 0, 0]
   },
 
   /**
@@ -36,8 +61,33 @@ Component({
         title: 'count的值：' + this.data.count,
         icon: "none"
       })
+    },
+    addRedValue() {
+      this.setData({
+        r: this.data.r + 1
+      })
+    },
+    addGreenValue() {
+      this.setData({
+        g: this.data.g + 1
+      })
+    },
+    addBlueValue() {
+      this.setData({
+        b: this.data.b + 1
+      })
+    },
+    _randomNumber: function() {
+      return Math.floor(Math.random()* (255 - 1 + 1) + 1)
     }
   },
+  observers: {
+    "r, g, b": function(r, g, b) {
+      this.setData({
+        allColors: [r, g, b]
+      })
+    }
+  }
   // options: {
   //   styleIsolation: "apply-shared"
   // }
